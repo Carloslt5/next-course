ad63ede8a3f77a4bfda1e7b093bb295a0f98ec1f
Carlos Liao <carloslt.proyectos@gmail.com>
2024-04-01 20:57:12 +0200

    ✅ add docker postgres data

---
+++ 03-admin-todo/src/app/api/hello/route.ts
@@ -0,0 +1,9 @@
+import { NextResponse } from "next/server";
+
+export async function GET(request: Request) {
+  return NextResponse.json({ hello: "mundo" });
+}
+
+export async function POST(request: Request) {
+  return NextResponse.json({ hello: "mundo", method: "POST" });
+}
