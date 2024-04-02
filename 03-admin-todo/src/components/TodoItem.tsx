af26d2dd2eabb3a7cfcadbe64021ad6394d840d4
Carlos Liao <carloslt.proyectos@gmail.com>
2024-04-02 18:04:20 +0200

    📦 task: update conditional color render todos

--- 03-admin-todo/src/components/TodoItem.tsx
+++ 03-admin-todo/src/components/TodoItem.tsx
@@ -1,16 +1,20 @@
 import { Todo } from "@prisma/client";
-import { IoCheckboxOutline } from "react-icons/io5";
+import { IoCheckboxOutline, IoSquareOutline } from "react-icons/io5";
 
 type TodoItemProps = {
   todo: Todo;
 };
+
 export const TodoItem = ({ todo }: TodoItemProps) => {
   return (
     <>
       <div className={todo.complete ? "todoDone" : "todoPending"}>
         <div className="flex flex-row w-full justify-start items-center gap-2">
-          <div className="flex p-2 rounded-md cursor-pointer hover:bg-opacity-60 bg-blue-100">
-            <IoCheckboxOutline size={30} />
+          <div
+            className={`flex p-2 rounded-md cursor-pointer hover:bg-opacity-60 bg-blue-100
+          ${todo.complete ? "bg-blue-100" : "bg-red-100"}`}
+          >
+            {todo.complete ? <IoCheckboxOutline size={30} /> : <IoSquareOutline size={30} />}
           </div>
           <div className="text-center sm:text-left">{todo.description}</div>
         </div>
