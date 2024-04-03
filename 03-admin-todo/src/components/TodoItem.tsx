143398d0f363a7187303e4e6af875a52077259f5
Carlos Liao <carloslt.proyectos@gmail.com>
2024-04-03 15:36:46 +0200

    📦 task: add optimistic hook

--- 03-admin-todo/src/components/TodoItem.tsx
+++ 03-admin-todo/src/components/TodoItem.tsx
@@ -1,4 +1,5 @@
 import { Todo } from "@prisma/client";
+import { useOptimistic } from "react";
 import { IoCheckboxOutline, IoSquareOutline } from "react-icons/io5";
 
 type TodoItemProps = {
@@ -7,18 +8,37 @@ type TodoItemProps = {
 };
 
 export const TodoItem = ({ todo, toggleTodo }: TodoItemProps) => {
+  const [todoOptimistic, setTodoOptimistic] = useOptimistic(
+    todo,
+    (state, newCompleteValue: boolean) => ({ ...state, complete: newCompleteValue })
+  );
+  const handleToggleTodo = async () => {
+    try {
+      setTodoOptimistic(!todoOptimistic.complete);
+      await toggleTodo(todo.id, !todo.complete);
+    } catch (error) {
+      setTodoOptimistic(!todoOptimistic.complete);
+    }
+  };
+
   return (
     <>
-      <div className={todo.complete ? "todoDone" : "todoPending"}>
+      <div className={todoOptimistic.complete ? "todoDone" : "todoPending"}>
         <div className="flex flex-row w-full justify-start items-center gap-2">
           <div
             className={`flex p-2 rounded-md cursor-pointer hover:bg-opacity-60 bg-blue-100
-          ${todo.complete ? "bg-blue-100" : "bg-red-100"}`}
-            onClick={() => toggleTodo(todo.id, !todo.complete)}
+          ${todoOptimistic.complete ? "bg-blue-100" : "bg-red-100"}`}
+            // without optimistic
+            // onClick={() => toggleTodo(todo.id, !todo.complete)}
+            onClick={handleToggleTodo}
           >
-            {todo.complete ? <IoCheckboxOutline size={30} /> : <IoSquareOutline size={30} />}
+            {todoOptimistic.complete ? (
+              <IoCheckboxOutline size={30} />
+            ) : (
+              <IoSquareOutline size={30} />
+            )}
           </div>
-          <div className="text-center sm:text-left">{todo.description}</div>
+          <div className="text-center sm:text-left">{todoOptimistic.description}</div>
         </div>
       </div>
     </>
