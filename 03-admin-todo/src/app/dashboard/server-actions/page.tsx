586854f9f88f52b27488ad434a41593a3c2df90a
Carlos Liao <carloslt.proyectos@gmail.com>
2024-04-03 13:47:19 +0200

    🚀 init server actions

--- 03-admin-todo/src/app/dashboard/server-actions/page.tsx
+++ 03-admin-todo/src/app/dashboard/server-actions/page.tsx
@@ -3,17 +3,16 @@ import { TodoGrid } from "@/components/TodoGrid";
 import prisma from "@/libs/prisma";
 
 export const metadata = {
-  title: "TODO List",
-  description: "All todos",
+  title: "TODO Server Actions",
+  description: "All todos server actions",
 };
 
-export default async function RestTodosPage() {
+export default async function ServerTodosPage() {
   const todos = await prisma.todo.findMany({ orderBy: { description: "asc" } });
 
   return (
     <>
-      <h1 className="text-4xl">Rest API TODOS</h1>
-
+      <h1 className="text-4xl">Server Actions</h1>
       <div className="mb-5">
         <NewTodo />
       </div>
