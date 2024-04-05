13a052067fd947f29105e08a4c0c440900d0aa90
Carlos Liao <carloslt.proyectos@gmail.com>
2024-04-05 21:05:41 +0200

    ✅ task: generate todo and user relation and update seed

--- 03-admin-todo/src/app/api/seed/route.ts
+++ 03-admin-todo/src/app/api/seed/route.ts
@@ -1,17 +1,26 @@
 import prisma from "@/libs/prisma";
+import bcrypt from "bcrypt";
 import { NextResponse } from "next/server";
 
 export async function GET(request: Request) {
   await prisma.todo.deleteMany();
+  await prisma.user.deleteMany();
 
-  await prisma.todo.createMany({
-    data: [
-      { description: "Learn NextJS" },
-      { description: "Learn Zustand" },
-      { description: "Read a chapter of a book" },
-      { description: "Clean the kitchen", complete: true },
-      { description: "Schedule dentist" },
-    ],
+  await prisma.user.create({
+    data: {
+      email: "test@email.com",
+      password: bcrypt.hashSync("1234", 10),
+      roles: ["admin"],
+      todos: {
+        create: [
+          { description: "Learn NextJS" },
+          { description: "Learn Zustand" },
+          { description: "Read a chapter of a book" },
+          { description: "Clean the kitchen", complete: true },
+          { description: "Schedule dentist" },
+        ],
+      },
+    },
   });
 
   return NextResponse.json({ message: "Seed execued" });
