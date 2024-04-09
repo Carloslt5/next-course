7b35b99ae44decf61fb5187971d78d3ab6ab8bcf
Carlos Liao <carloslt.proyectos@gmail.com>
2024-04-09 17:10:03 +0200

    ✅ task: add product in product page by slug

--- 04-teslo-shop/src/app/(shop)/product/[slug]/page.tsx
+++ 04-teslo-shop/src/app/(shop)/product/[slug]/page.tsx
@@ -1,4 +1,20 @@
-export default function ProductPage() {
+import { initialData } from "@/seed/seed";
+import { notFound } from "next/navigation";
+
+type ProductPageProps = {
+  params: {
+    slug: string;
+  };
+};
+
+export default function ProductPage({ params }: ProductPageProps) {
+  const { slug } = params;
+  const product = initialData.products.find((product) => product.slug === slug);
+
+  if (!product) {
+    notFound();
+  }
+
   return (
     <div>
       <h1>Product page</h1>
