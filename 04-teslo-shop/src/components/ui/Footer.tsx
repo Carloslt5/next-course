d3f3f7cf7f33d6bf6d7bef708dde037d8a35585b
Carlos Liao <carloslt.proyectos@gmail.com>
2024-04-12 15:16:13 +0200

    🔨 task: add footer

---
+++ 04-teslo-shop/src/components/ui/Footer.tsx
@@ -0,0 +1,21 @@
+import Link from "next/link";
+
+export const Footer = () => {
+  return (
+    <div className="flex w-full justify-center text-xs gap-3 py-4">
+      <Link href={"/"}>
+        <span className="font-bold">Teslo</span>
+        <span> | shop </span>
+        <span>©{new Date().getFullYear()}</span>
+      </Link>
+
+      <Link href={"/"}>
+        <span>Privacy and legal</span>
+      </Link>
+
+      <Link href={"/"}>
+        <span>Privacy and legal</span>
+      </Link>
+    </div>
+  );
+};
