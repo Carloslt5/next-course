9a2cc8c9c8bd5e603a0383256913581eeeca958c
Carlos Liao <carloslt.proyectos@gmail.com>
2024-04-18 15:34:06 +0200

    🔨 task: config auth providers

--- 04-teslo-shop/auth.config.ts
+++ 04-teslo-shop/auth.config.ts
@@ -1,9 +1,27 @@
-import type { NextAuthConfig } from "next-auth";
+import NextAuth, { type NextAuthConfig } from "next-auth";
+import credentials from "next-auth/providers/credentials";
+import { z } from "zod";
 
 export const authConfig: NextAuthConfig = {
   pages: {
     signIn: "/auth/login",
     newUser: "/auth/signup",
   },
-  providers: [],
+  providers: [
+    credentials({
+      async authorize(credentials) {
+        const parsedCredentials = z
+          .object({ email: z.string().email(), password: z.string().min(6) })
+          .safeParse(credentials);
+        if (!parsedCredentials.success) return null;
+
+        const { email, password } = parsedCredentials.data;
+        console.log("🚀 --------- data", { email, password });
+
+        return null;
+      },
+    }),
+  ],
 };
+
+export const { signIn, signOut, auth } = NextAuth(authConfig);
