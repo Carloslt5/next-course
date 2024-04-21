dfb26728f7b5f4ee3a06d5c7219ba5ee39f57427
Carlos Liao <carloslt.proyectos@gmail.com>
2024-04-21 19:41:09 +0200

    🔨 task: register user with server action

---
+++ 04-teslo-shop/src/actions/auth/register.ts
@@ -0,0 +1,32 @@
+"use server";
+
+import prisma from "@/lib/prisma";
+import bcrypt from "bcrypt";
+
+export const registerUser = async (name: string, email: string, password: string) => {
+  try {
+    const user = await prisma.user.create({
+      data: {
+        name: name,
+        password: bcrypt.hashSync(password, 10),
+        email: email.toLowerCase(),
+      },
+      select: {
+        id: true,
+        name: true,
+        email: true,
+      },
+    });
+    return {
+      status: true,
+      messgge: "Created user",
+      data: user,
+    };
+  } catch (error) {
+    console.log("🚀 --------- error", error);
+    return {
+      status: false,
+      messgge: "Can not created user",
+    };
+  }
+};
--- 04-teslo-shop/src/components/auth/RegisterForm.tsx
+++ 04-teslo-shop/src/components/auth/RegisterForm.tsx
@@ -1,6 +1,9 @@
 "use client";
 
+import { registerUser } from "@/actions/auth/register";
+import clsx from "clsx";
 import Link from "next/link";
+import { useState } from "react";
 import { SubmitHandler, useForm } from "react-hook-form";
 
 type FormInputs = {
@@ -10,45 +13,63 @@ type FormInputs = {
 };
 
 export const RegisterForm = () => {
+  const [errorMessage, setErrorMessage] = useState("");
+
   const {
     register,
     handleSubmit,
     formState: { errors },
   } = useForm<FormInputs>();
 
-  const onSubmit: SubmitHandler<FormInputs> = (data) => {
+  const onSubmit: SubmitHandler<FormInputs> = async (data) => {
+    setErrorMessage("");
+    console.log("🚀 --------- data", data);
+
     const { name, email, password } = data;
-    console.log("🚀 --------- name", { name, email, password });
+    const res = await registerUser(name, email, password);
+    if (!res.status) {
+      setErrorMessage(res.messgge);
+      return;
+    }
+    console.log("🚀 --------- res", res);
   };
 
   return (
     <form className="flex flex-col w-full" onSubmit={handleSubmit(onSubmit)}>
       <label htmlFor="name">Full Name</label>
       <input
-        className="px-5 py-2 border bg-gray-200 rounded mb-5"
+        className={clsx("px-5 py-2 border bg-gray-200 rounded mb-5", {
+          "border-red-500": errors.name,
+        })}
         type="text"
-        {...register("name")}
+        {...register("name", { required: true })}
         autoFocus
-        required
       />
 
       <label htmlFor="email">Email</label>
       <input
-        className="px-5 py-2 border bg-gray-200 rounded mb-5"
+        className={clsx("px-5 py-2 border bg-gray-200 rounded mb-5", {
+          "border-red-500": errors.email,
+        })}
         type="email"
-        {...register("email")}
+        {...register("email", {
+          required: true,
+          pattern: /^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$/,
+        })}
         autoFocus
-        required
       />
 
       <label htmlFor="password">Password</label>
       <input
-        className="px-5 py-2 border bg-gray-200 rounded mb-5"
+        className={clsx("px-5 py-2 border bg-gray-200 rounded mb-5", {
+          "border-red-500": errors.password,
+        })}
         type="password"
-        {...register("password")}
+        {...register("password", { required: true, minLength: 4 })}
         autoFocus
-        required
       />
+
+      <span className="text-red-500 mb-4">{errorMessage}</span>
 
       <button type="submit" className="btn-primary">
         Sign up
