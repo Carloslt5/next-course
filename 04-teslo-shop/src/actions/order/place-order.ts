243accb976441f1a405b3b23d27202b7e79408bc
Carlos Liao <carloslt.proyectos@gmail.com>
2024-04-30 19:41:20 +0200

    🔨 task: generate subTotal, tax and total in server-action

--- 04-teslo-shop/src/actions/order/place-order.ts
+++ 04-teslo-shop/src/actions/order/place-order.ts
@@ -3,6 +3,7 @@
 import { auth } from "@/auth";
 import { Address } from "@/interfaces/address.type";
 import { Size } from "@/interfaces/product.type";
+import prisma from "@/lib/prisma";
 
 interface ProductOrder {
   productId: string;
@@ -21,6 +22,32 @@ export const placeOrder = async (productIds: ProductOrder[], address: Address) =
     };
   }
 
-  console.log("🚀 --------- session", userId);
-  console.log({ productIds, address });
+  const products = await prisma.product.findMany({
+    where: {
+      id: {
+        in: productIds.map((item) => item.productId),
+      },
+    },
+  });
+
+  const itemsInOrder = productIds.reduce((count, product) => count + product.quantity, 0);
+
+  const { subTotal, tax, total } = productIds.reduce(
+    (totals, item) => {
+      const productQuantity = item.quantity;
+      const product = products.find((product) => product.id === item.productId);
+
+      if (!product) throw new Error(`${item.productId} not exitst - 500`);
+
+      const subtotal = product.price * productQuantity;
+
+      totals.subTotal += subtotal;
+      totals.tax += subtotal * 0.15;
+      totals.total += subtotal * 1.15;
+
+      return totals;
+    },
+    { subTotal: 0, tax: 0, total: 0 }
+  );
+  console.log("🚀 --------- { subTotal, tax, total }", { subTotal, tax, total });
 };
