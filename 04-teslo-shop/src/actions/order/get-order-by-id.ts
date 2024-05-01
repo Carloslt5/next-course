26ed01ab414721e179430bb870f1f6a3fe166516
Carlos Liao <carloslt.proyectos@gmail.com>
2024-05-01 20:32:58 +0200

    🔨 get order byid with action server and display in orderID page

---
+++ 04-teslo-shop/src/actions/order/get-order-by-id.ts
@@ -0,0 +1,61 @@
+"use server";
+
+import { auth } from "@/auth";
+import prisma from "@/lib/prisma";
+
+export const getOrderByID = async (id: string) => {
+  const session = await auth();
+  if (!session) {
+    return {
+      status: false,
+      message: "Need to authenticate",
+    };
+  }
+
+  try {
+    const order = await prisma.order.findUnique({
+      where: {
+        id: id,
+      },
+      include: {
+        OrderAddress: true,
+        OrderItem: {
+          select: {
+            price: true,
+            quantity: true,
+            size: true,
+            product: {
+              select: {
+                title: true,
+                slug: true,
+                ProductImage: {
+                  select: {
+                    url: true,
+                  },
+                  take: 1,
+                },
+              },
+            },
+          },
+        },
+      },
+    });
+
+    if (!order) throw `Order ${id} not exist`;
+    if (session.user.role === "user") {
+      if (session.user.id !== order.userId) {
+        throw `Order ${id} not belong to the ${session.user.id}`;
+      }
+    }
+
+    return {
+      status: true,
+      order: order,
+    };
+  } catch (error) {
+    return {
+      status: false,
+      message: "order not exist",
+    };
+  }
+};
--- 04-teslo-shop/src/app/(shop)/orders/[id]/page.tsx
+++ 04-teslo-shop/src/app/(shop)/orders/[id]/page.tsx
@@ -1,11 +1,11 @@
+import { getOrderByID } from "@/actions/order/get-order-by-id";
 import { Title } from "@/components/ui/Title";
-import { PAID } from "@/constants/OrderStatus.const";
-import { initialData } from "@/seed/seed";
+import { PAID, PENDING_PAYMENT } from "@/constants/OrderStatus.const";
+import { currencyFormat } from "@/utils/currencyFormat";
 import clsx from "clsx";
 import Image from "next/image";
+import { redirect } from "next/navigation";
 import { IoCardOutline } from "react-icons/io5";
-
-const productInCart = [initialData.products[0], initialData.products[1], initialData.products[2]];
 
 type OrderPageProps = {
   params: {
@@ -13,12 +13,19 @@ type OrderPageProps = {
   };
 };
 
-export default function OrderPage({ params }: OrderPageProps) {
+export default async function OrderPage({ params }: OrderPageProps) {
   const { id } = params;
+
+  const { status, order } = await getOrderByID(id);
+
+  if (!status) {
+    redirect("/");
+  }
+
   return (
     <div className="flex justify-center items-center px-1 ">
       <div className="flex flex-col w-[1400px] ">
-        <Title title={`Order #${id}`} />
+        <Title title={`Order #${id.split("-").at(-1)}`} />
 
         <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
           <div className="flex flex-col gap-4">
@@ -26,33 +33,36 @@ export default function OrderPage({ params }: OrderPageProps) {
               className={clsx(
                 "flex gap-2 items-center rounded-lg py-2 px-3 text-xs font-bold text-white mb-2",
                 {
-                  "bg-red-300": false,
-                  "bg-green-700": true,
+                  "bg-red-500": !order?.isPaid,
+                  "bg-green-700": order?.isPaid,
                 }
               )}
             >
               <IoCardOutline size={30} />
-              {/* <span>{ PENDING_PAYMENT}t</span> */}
-              <span>{PAID}</span>
+              <span>{order?.isPaid ? PAID : PENDING_PAYMENT}</span>
             </div>
 
-            {productInCart.map((product) => (
-              <div key={product.slug} className="flex flex-row gap-2">
+            {order?.OrderItem.map((item) => (
+              <div key={item.product.slug + "-" + item.size} className="flex flex-row gap-2">
                 <figure className=" w-1/3 max-w-[100px] bg-green-100 rounded-md overflow-hidden">
                   <Image
-                    src={`/products/${product.images[0]}`}
+                    src={`/products/${item.product.ProductImage[0].url}`}
                     width={120}
                     height={120}
                     style={{ objectFit: "cover", width: "100%", height: "100%" }}
-                    alt={product.title}
+                    alt={item.product.title}
                     className="rounded-md"
                     priority
                   />
                 </figure>
                 <article className="w-2/3">
-                  <p>{product.title}</p>
-                  <p>${product.price} x 3</p>
-                  <p className="font-bold">Subtotal: ${product.price * 3}</p>
+                  <p>{item.product.title}</p>
+                  <p>
+                    ${item.price} x {item.quantity}
+                  </p>
+                  <p className="font-bold">
+                    Subtotal: ${currencyFormat(item.price * item.quantity)}
+                  </p>
                 </article>
               </div>
             ))}
@@ -62,12 +72,16 @@ export default function OrderPage({ params }: OrderPageProps) {
             <div>
               <h2 className="mb-2 text-2xl font-bold">Delivery address</h2>
               <article>
-                <p className="text-2xl">Name Last name</p>
-                <p>Address</p>
-                <p>City</p>
-                <p>Country</p>
-                <p>ZIP code</p>
-                <p>phone</p>
+                <p className="text-2xl">
+                  {order?.OrderAddress?.name} {order?.OrderAddress?.lastName}
+                </p>
+                <p>{order?.OrderAddress?.address}</p>
+                <p>{order?.OrderAddress?.city}</p>
+                <p>
+                  {order?.OrderAddress?.city}, {order?.OrderAddress?.countryId}
+                </p>
+                <p>{order?.OrderAddress?.zipCode}</p>
+                <p>{order?.OrderAddress?.phone}</p>
               </article>
             </div>
             <hr />
@@ -75,30 +89,31 @@ export default function OrderPage({ params }: OrderPageProps) {
               <h2 className="mb-2 text-2xl font-bold">Order Summary</h2>
               <article className="grid grid-cols-2">
                 <span>Product Nº</span>
-                <span className="text-right">Quantity</span>
+                <span className="text-right">{order?.itemsInOrder} items</span>
 
                 <span>Subtotal</span>
-                <span className="text-right">$100</span>
+                <span className="text-right">{currencyFormat(order!.subTotal)}</span>
 
                 <span>Taxes 15%</span>
-                <span className="text-right">$100</span>
+                <span className="text-right">{currencyFormat(order!.tax)}</span>
 
                 <span className="mt-5 font-bold text-xl ">Total:</span>
-                <span className="mt-5 font-bold text-xl text-right">$100</span>
+                <span className="mt-5 font-bold text-xl text-right">
+                  {currencyFormat(order!.total)}
+                </span>
               </article>
             </div>
             <div
               className={clsx(
-                "flex gap-2 items-center rounded-lg py-2 px-3 text-xs font-bold text-white",
+                "flex gap-2 items-center rounded-lg py-2 px-3 text-xs font-bold text-white mb-2",
                 {
-                  "bg-red-300": false,
-                  "bg-green-700": true,
+                  "bg-red-500": !order?.isPaid,
+                  "bg-green-700": order?.isPaid,
                 }
               )}
             >
               <IoCardOutline size={30} />
-              {/* <span>{ PENDING_PAYMENT}</span> */}
-              <span>{PAID}</span>
+              <span>{order?.isPaid ? PAID : PENDING_PAYMENT}</span>
             </div>
           </div>
         </div>
