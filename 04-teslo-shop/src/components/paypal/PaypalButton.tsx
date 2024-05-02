a6b6429681d97349270533038e5a4c21fce263b4
Carlos Liao <carloslt.proyectos@gmail.com>
2024-05-02 17:45:29 +0200

    🔨 task: generate paypal skeleton button

--- 04-teslo-shop/src/components/paypal/PaypalButton.tsx
+++ 04-teslo-shop/src/components/paypal/PaypalButton.tsx
@@ -1,7 +1,18 @@
 "use client";
 
-import { PayPalButtons } from "@paypal/react-paypal-js";
+import { PayPalButtons, usePayPalScriptReducer } from "@paypal/react-paypal-js";
 
 export const PaypalButton = () => {
+  const [{ isPending }] = usePayPalScriptReducer();
+
+  if (isPending) {
+    return (
+      <div className="animate-pulse flex flex-col gap-5">
+        <div className="h-12 bg-gray-300 rounded" />
+        <div className="h-12 bg-gray-300 rounded" />
+      </div>
+    );
+  }
+
   return <PayPalButtons />;
 };
