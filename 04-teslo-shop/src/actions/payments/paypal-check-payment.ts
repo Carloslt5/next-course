9a241d41dbf3c4355951cbe6a7cabace74ee80e2
Carlos Liao <carloslt.proyectos@gmail.com>
2024-05-03 20:02:24 +0200

    🔨 task: add verify payment from paypal with OAuth token

--- 04-teslo-shop/src/actions/payments/paypal-check-payment.ts
+++ 04-teslo-shop/src/actions/payments/paypal-check-payment.ts
@@ -1,17 +1,35 @@
 "use server";
 
+import { PaypalStatusOrderResponse } from "@/interfaces/paypal.type";
+
 export const paypalCheckPayment = async (transactionId: string) => {
   const authToken = await getPayPalBearerToken();
-  console.log("🚀 --------- authToken", authToken);
   if (!authToken) {
     return {
       status: false,
       message: "Can not get Auth Token",
     };
   }
+
+  const resp = await verifyPaypalPayment(transactionId, authToken);
+  if (!resp) {
+    return {
+      status: false,
+      message: "Error verify payment",
+    };
+  }
+  const { status, purchase_units } = resp;
+  console.log("🚀 --------- status", { status, purchase_units });
+  if (status !== "COMPLETED") {
+    return {
+      status: false,
+      message: "The order has not yet been paid in PayPal",
+    };
+  }
   return;
 };
 
+// Generate Oauth Token from Paypal
 const getPayPalBearerToken = async (): Promise<string | null> => {
   const base64Token = Buffer.from(
     `${process.env.NEXT_PUBLIC_PAYPAL_CLIENT_ID}:${process.env.PAYPAL_SECRET}`,
@@ -36,6 +54,31 @@ const getPayPalBearerToken = async (): Promise<string | null> => {
       (response) => response.json()
     );
     return response.access_token;
+  } catch (error) {
+    console.log(error);
+    return null;
+  }
+};
+
+// Verify order payment
+const verifyPaypalPayment = async (
+  transactionId: string,
+  bearerToken: string
+): Promise<PaypalStatusOrderResponse | null> => {
+  const myHeaders = new Headers();
+  myHeaders.append("Authorization", `Bearer ${bearerToken}`);
+
+  const requestOptions = {
+    method: "GET",
+    headers: myHeaders,
+  };
+
+  try {
+    const response = await fetch(
+      `${process.env.PAYPAL_ORDERS_URL}/${transactionId}`,
+      requestOptions
+    ).then((response) => response.json());
+    return response;
   } catch (error) {
     console.log(error);
     return null;
